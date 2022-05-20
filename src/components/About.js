63e00f8a0bf2933ecc9606ba17fc0bf9bd0da364
Davi Mury <davi.j.mury@gmail.com>
2022-05-20 00:57:10 -0300

    About me and first changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import pythonIcon from "@iconify/icons-logos/python";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import dockerIcon from "@iconify/icons-logos/docker-icon";
 
 class About extends Component {
   render() {
@@ -11,6 +11,7 @@ class About extends Component {
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var resumeText = this.props.resumeBasicInfo.resumeText;
       var hello = this.props.resumeBasicInfo.description_header;
       var about = this.props.resumeBasicInfo.description;
     }
@@ -31,21 +32,34 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={pythonIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%", maxWidth: '50px' }}
                   />
                   <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%", maxWidth: '50px' }}
                   />
                   <Icon
-                    icon={vueIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={dockerIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%", maxWidth: '50px' }}
                   />
+                  <div className="mt-4">
+                    <button className="button">
+                      <svg id="icon" x="0px" y="0px" viewBox="0 0 490 490">
+                        <g>
+                          <polygon points="360.352,232.334 324.344,203.803 267.969,274.968 267.969,0 222.031,0 222.031,274.968 165.656,203.803 
+                            129.648,232.334 245,377.923 	"/>
+                          <rect x="58.02" y="444.063" width="373.96" height="45.938"/>
+                        </g>
+                      </svg>
+                      <div className="text">
+                        {resumeText}
+                      </div>
+                  </button>
+                  </div>
                 </span>
               </div>
             </div>
-
             <div className="col-md-8 center">
               <div className="col-md-10">
                 <div className="card">
@@ -74,6 +88,7 @@ class About extends Component {
                       height: "auto",
                       fontSize: "132%",
                       lineHeight: "200%",
+                      whiteSpace: 'pre-line',
                     }}
                   >
                     <br />
